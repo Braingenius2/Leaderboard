5267ad10d04e0a9f18e266ad2320a9a4dbb68e8e
Braingenius2 <braingenius2@gmail.com>
2023-05-27 22:20:03 +0300

    Add functionality to refresh button

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const sendRequest = async (url, method = 'GET', data = {}) => {
   }
 };
 
-// Create a new game using the API
+// Create a new game
 const createGame = async (name) => {
   const url = baseURL + 'games/';
   const data = { name };
@@ -34,14 +34,14 @@ const createGame = async (name) => {
   return result;
 };
 
-// Get scores for a specific game from the API
+// Get scores for a specific game
 const getScores = async (gameId) => {
   const url = baseURL + `games/${gameId}/scores/`;
   const result = await sendRequest(url);
   return result;
 };
 
-// Submit a new score for a specific game using the API
+// Submit a new score for a specific game
 const submitScore = async (gameId, user, score) => {
   const url = baseURL + `games/${gameId}/scores/`;
   const data = { user, score };
@@ -49,3 +49,19 @@ const submitScore = async (gameId, user, score) => {
   const result = await sendRequest(url, 'POST', data);
   return result;
 };
+
+// Event listener for refresh button
+const refreshButton = document.querySelector('.refresh');
+refreshButton.addEventListener('click', async () => {
+  const gameId = 'FortuneuzodinmA';
+  const scores = await getScores(gameId);
+  const scoreCard = document.querySelector('.scorecard');
+  scoreCard.innerHTML = '';
+  scores.result.forEach((entry) => {
+    const div = document.createElement('div');
+    div.innerHTML = `<span>Name: </span><span>${entry.score}</span>`;
+    scoreCard.appendChild(div);
+  });
+});
+
+
